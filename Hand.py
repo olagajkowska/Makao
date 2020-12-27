ef8c1394633c37bb8b0ec0e731fed62034005998
olagajkowska <a.gajkowska@cent.uw.edu.pl>
2020-12-27 01:58:47 +0100

    new methods: play_card, sort_hand, draw

--- Hand.py
+++ Hand.py
@@ -1,14 +1,42 @@
-from Base_dictionary import Base_dictionary
+from Base_container import Base_container
 
 
-class Hand(Base_dictionary):
-    __slots__ = ['__hand']
+class Hand(Base_container):
+    __slots__ = []
 
     def __init__(self):
         super().__init__()
-        self.__hand = self.add_keys(['♣', '♦', '♥', '♠'])
 
     def __str__(self):
-        for key, cards in self.__hand.items():
-            print(*cards)
+        for item in self.content:
+            print(item)
         return ''
+
+    def sort_hand(self):
+        srt = {b: i for i, b in enumerate(['♣', '♦', '♥', '♠'])}
+        self.content.sort(key=lambda x: x.value_rank)
+        self.content.sort(key=lambda x: srt[x.suit[0]])
+        new_id = 0
+        for card in self.content:
+            card.set_id(new_id)
+            new_id += 1
+
+    def play_card(self, ok_cards, stack):
+        self.remove(ok_cards[0].id)  # tutaj strategia powinna wybrać odpowiednią kartę z ok_cards
+        stack.add(ok_cards[0])
+        new_id = 0
+        for card in self.content:
+            card.set_id(new_id)
+            new_id += 1
+        print('card: ', ok_cards[0], '\n')
+
+    def draw(self, deck):
+        card = deck.get_top
+        deck.remove(card.id)
+        self.add(card)
+
+
+
+
+
+
