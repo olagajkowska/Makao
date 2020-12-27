b88fa95cd956313ebc1c6f1e7193373c6061a150
olagajkowska <a.gajkowska@cent.uw.edu.pl>
2020-12-27 02:00:52 +0100

    minor changes

--- Makao.py
+++ Makao.py
@@ -1,6 +1,4 @@
 from Game import Game
 
 if __name__ == "__main__":
-    value_list = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-    color_list = ['♣', '♦', '♥', '♠']
-    play = Game(value_list, color_list, N_Players = 2, N_Decks = 1)
+    play = Game(N_Players=4, N_Decks=1)
